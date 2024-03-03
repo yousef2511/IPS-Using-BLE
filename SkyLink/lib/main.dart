e18f61b711b9b3416a2a39752f330fd9acc6a0f6
yousef2511 <tarekyousef942@gmail.com>
2024-03-03 17:21:55 +0200

    indoor position system

---
+++ SkyLink/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:f2/Login_page.dart';
+import 'package:f2/newB.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
